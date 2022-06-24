cbdba8566bac3de14c0eb4f85da734757e44f6a6
tobby-t <105219273+tobby-t@users.noreply.github.com>
2022-06-24 10:07:36 -0700

    Myself with only javascript,first push

---
+++ script.js
@@ -0,0 +1,6 @@
+let  mySelf = {
+    fname: "Tobiloba Lawal",
+    height:  "165cm",
+    country: "Nigeria",
+};
+document.write ("My name is " + mySelf.fname + ','  + " my height is " + mySelf.height + ',' + " and I come from " + mySelf.country + '.');
